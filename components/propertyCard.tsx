7f379255a0d38500102c1e2be271e47437d85a59
UgustinV <augustin.viard0@gmail.com>
2026-09-15 16:33:20 +0200

    added fav button to properties list & about page done

--- components/propertyCard.tsx
+++ components/propertyCard.tsx
@@ -4,8 +4,11 @@ import Image from "next/image";
 
 export const PropertyCard = ({ property }: { property: PropertyBase }) => {
     return (
-        <Link className="bg-white rounded-b-[10px]" href={`/logements/${property.id}`}>
+        <Link className="relative bg-white rounded-b-[10px]" href={`/logements/${property.id}`}>
             <Image src={property.cover ?? "/default-cover.jpg"} alt={property.title} className="w-full h-60 object-cover rounded-t-[10px] pb-4" width={400} height={300} />
+            <button className="absolute top-4 right-4 bg-(--light-grey) p-2 rounded-[5px] w-8 h-8">
+                <Image src="/favoris.svg" alt={property.title} width={16} height={16} />
+            </button>
             <div className="px-6 pb-6">
                 <h2 className="text-lg font-bold text-(--dark-grey)">{property.title}</h2>
                 <p className="text-sm text-gray-500 mb-15.5">{property.location}</p>
